aa956494ec758fad39f37abc366ada7b3b2bfd1f
PhamQuan <bestzed12345ff@gmail.com>
2024-10-27 00:00:00 +0700

    Add payment
    Add payment

---
+++ Ticket_Hub/Ticket_Hub.Models/DTO/Payment/PaymentInformationDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Ticket_Hub.Models.DTO.Payment
+{
+    public class PaymentInformationDto
+    {
+        public string CardId { get; set; }
+        public string OrderType { get; set; }
+        public string OrderDescription { get; set; }
+        public string Name { get; set; }
+        public double Amount { get; set; }
+    }
+}
